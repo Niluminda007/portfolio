88624a7c94f0ebec1e75fc28f1a2d36ed3baac39
Ushan Niluminda Jayawardana <princeofpersiatwathrones@gmail.com>
2023-04-13 12:24:14 +0300

    some changes

--- src/components/AnimatedProjectCard/AnimatedProjectCard.jsx
+++ src/components/AnimatedProjectCard/AnimatedProjectCard.jsx
@@ -15,8 +15,8 @@ export const AnimatedProjectCard = ({ item: { url, name, path } }) => {
             ? "-translate-x-[40%] top-[14rem]"
             : isTablet
             ? "-translate-x-[65%] bottom-[10rem]"
-            : "-translate-x-[70%] bottom-[8rem]"
-        } uppercase cursor-default text-white text-center -rotate-90  text-4xl absolute animate-text-animation`}
+            : "-translate-x-[70%] bottom-[8rem] animate-text-animation"
+        } uppercase cursor-default text-white text-center -rotate-90  text-4xl absolute`}
       />
       <Link to={path}>
         <div
@@ -41,8 +41,8 @@ export const AnimatedProjectCard = ({ item: { url, name, path } }) => {
           ></div>
           <div
             className={`${
-              isMobile ? "" : isTablet ? "" : "animate-screen-animation"
-            } screen__overlay absolute z-[2] left-0 top-0   bg-9 screen-gradient h-[100%] w-[100%]`}
+              isMobile ? "" : isTablet ? "" : ""
+            } screen__overlay absolute z-[2] left-0 top-0   bg-9 screen-gradient h-[100%] w-[100%] animate-screen-animation`}
           ></div>
           <div className="screen-content relative z-[3] w-[95%] h-[95%] aspect-12/16 border-solid border-[1px] border-[rgba(128,43,177,.5)] rounded-[0.6rem] pb-[6rem] m-4"></div>
           <span className="absolute bg-white w-[25%] h-[5px] z-[4] left-[50%] -translate-x-[50%] translate-y-[0%] bottom-0 rounded-tr-[1rem] rounded-tl-[1rem]"></span>
