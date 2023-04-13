b37c697a41aac177ebc7cbf962e89833a998826c
Ushan Niluminda Jayawardana <princeofpersiatwathrones@gmail.com>
2023-04-13 10:33:40 +0300

    adjusted porject page for mobile and tablet

--- src/components/AnimatedProjectCard/AnimatedProjectCard.jsx
+++ src/components/AnimatedProjectCard/AnimatedProjectCard.jsx
@@ -1,17 +1,33 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
 import { SpecialHeading } from "../SpecialHeading/SpecialHeading";
+import { AppContext } from "../../context/context";
 
 export const AnimatedProjectCard = ({ item: { url, name, path } }) => {
+  const { isMobile, isTablet } = useContext(AppContext);
   return (
     <>
       <SpecialHeading
         isNormal={true}
         heading={{ nav_name: name, path: path }}
-        styleClass="uppercase cursor-default text-white text-center -rotate-90  text-4xl absolute -translate-x-[70%] bottom-[8rem] animate-text-animation"
+        styleClass={`${
+          isMobile
+            ? "-translate-x-[40%] top-[14rem]"
+            : isTablet
+            ? "-translate-x-[65%] bottom-[10rem]"
+            : "-translate-x-[70%] bottom-[8rem]"
+        } uppercase cursor-default text-white text-center -rotate-90  text-4xl absolute animate-text-animation`}
       />
       <Link to={path}>
-        <div className="screen relative overflow-hidden w-[450px] aspect-[12/16] border-solid border-3 border-[rgba(128,43,177,.7)] rounded-2xl bg bg-[rgba(128,43,177,.15)]">
+        <div
+          className={`${
+            isMobile
+              ? "w-[75vw] aspect-[10/16] ml-16"
+              : isTablet
+              ? "w-[250px] aspect-[10/16]"
+              : "w-[450px] aspect-[12/16]"
+          } screen relative overflow-hidden  border-solid border-3 border-[rgba(128,43,177,.7)] rounded-2xl bg bg-[rgba(128,43,177,.15)]`}
+        >
           <span className="absolute bg-white w-[15%] h-[5px] z-[4] left-[50%] -translate-x-[50%] translate-y-[0%] top-0 rounded-bl-[1rem] rounded-br-[1rem]"></span>
           <div
             style={{
--- src/components/ProjectsScroll/ProjectsScroll.jsx
+++ src/components/ProjectsScroll/ProjectsScroll.jsx
@@ -7,13 +7,14 @@ import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 gsap.registerPlugin(ScrollTrigger);
 export const ProjectsScroll = () => {
-  const { toggleMenu } = useContext(AppContext);
+  const { toggleMenu, isMobile, isTablet } = useContext(AppContext);
   const rootRef = useRef(null);
   useEffect(() => {
     const ctx = gsap.context(() => {
       const sections = gsap.utils.toArray(".container section");
       gsap.to(sections, {
-        xPercent: -100 * (sections.length - 1),
+        xPercent:
+          (isMobile ? -120 : isTablet ? -120 : -100) * (sections.length - 1),
         ease: "none",
         scrollTrigger: {
           trigger: ".wrap",
@@ -42,10 +43,24 @@ export const ProjectsScroll = () => {
         }  fixed top-[7.5rem] bg-[dark] flex gap-10  justify-center items-center`}
       >
         <div className="wrapper overflow-x-hidden relative h-[100vh] w-[100vw]">
-          <div ref={rootRef} className="container w-[240vw] h-[80vh] flex">
+          <div
+            ref={rootRef}
+            className={`${
+              isMobile ? "w-[360vw]" : isTablet ? "w-[360vw]" : " w-[240vw]"
+            } container h-[80vh] flex`}
+          >
             {projectsCoverDetails.map((item, index) => {
               return (
-                <section key={index} className="w-[60vw] px-[20vw] py-[8vw]">
+                <section
+                  key={index}
+                  className={`${
+                    isMobile
+                      ? "w-[90vw] px-[5vw] py-[8vw] mr-8 my-[5rem]"
+                      : isTablet
+                      ? "w-[90vw] px-[5vw] py-[4vw] ml-32"
+                      : "w-[60vw] px-[20vw] py-[8vw]"
+                  }`}
+                >
                   <AnimatedProjectCard item={item} />
                 </section>
               );
