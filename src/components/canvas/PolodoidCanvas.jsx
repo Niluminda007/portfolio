e664be932956754e2cf9a9567a45cb52f686c8d6
Ushan Niluminda Jayawardana <princeofpersiatwathrones@gmail.com>
2023-04-14 19:52:51 +0300

    made some changes

--- src/components/canvas/PolodoidCanvas.jsx
+++ src/components/canvas/PolodoidCanvas.jsx
@@ -1,34 +1,37 @@
-import React, { useRef } from "react";
+import React, { useRef, Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { Environment, useGLTF, OrbitControls } from "@react-three/drei";
+import CanvasLoader from "../CanvasLoader/CanvasLoader";
 
 const PolodoidPhoto = () => {
   const photo = useGLTF("./polaroid/Sketchfab_Scene.glb");
   return (
-    <>
-      <Environment preset="warehouse" />
+    <mesh>
+      <pointLight intensity={0.6} />
       <primitive
         object={photo.scene}
         scale={0.4}
         position={[0, -4, -10]}
         rotation={[1, 0.7, -0.008]}
       />
-    </>
+    </mesh>
   );
 };
 
 export const PolodoidCanvas = () => {
   return (
     <Canvas frameloop="demand" shadows gl={{ preserveDrawingBuffer: true }}>
-      <perspectiveCamera position={[1, 1, 1]} fov={45} near={1000} />
-      <OrbitControls
-        maxPolarAngle={Math.PI / 2}
-        minPolarAngle={Math.PI / 2}
-        minAzimuthAngle={-Math.PI / 3}
-        maxAzimuthAngle={Math.PI / 3}
-        enableZoom={false}
-      />
-      <PolodoidPhoto />
+      <Suspense fallback={<CanvasLoader />}>
+        <perspectiveCamera position={[1, 1, 1]} fov={45} near={1000} />
+        <OrbitControls
+          maxPolarAngle={Math.PI / 2}
+          minPolarAngle={Math.PI / 2}
+          minAzimuthAngle={-Math.PI / 3}
+          maxAzimuthAngle={Math.PI / 3}
+          enableZoom={false}
+        />
+        <PolodoidPhoto />
+      </Suspense>
     </Canvas>
   );
 };
